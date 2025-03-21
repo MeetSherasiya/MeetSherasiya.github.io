1dae69d34246b4fb4fd955097e8e58758b7c1fe5
Meet <meetsherasiya2512@gmail.com>
2025-03-21 15:30:52 +0530

    Add : Js File Plugins

--- js/main.js
+++ js/main.js
@@ -108,10 +108,6 @@ function setREVStartSize(e) {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    setTimeout(() => {
-        document.getElementById('page').style.display = 'block'; 
-        // document.body.style.display = "block";
-    }, 1000);
     const marqueeWrap = document.querySelector(".marquee-list-wrap");
     const marqueeList = document.querySelector(".marquee-list");
 
