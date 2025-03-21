f02369aa77e1c6a4c47f3921d046234183b78cd8
Meet Sherasiya <114209655+MeetSherasiya@users.noreply.github.com>
2025-03-22 02:43:06 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,19 +6,19 @@ var dataLayer_content = {
 };
 dataLayer.push(dataLayer_content);
 
-(function (w, d, s, l, i) {
-    w[l] = w[l] || []; 
-    w[l].push({
-        'gtm.start': new Date().getTime(), 
-        event: 'gtm.js'
-    }); 
-    var f = d.getElementsByTagName(s)[0],
-        j = d.createElement(s), 
-        dl = l != 'dataLayer' ? '&l=' + l : ''; 
-    j.async = true; 
-    j.src = '//www.googletagmanager.com/gtm.js?id=' + i + dl; 
-    f.parentNode.insertBefore(j, f);
-})(window, document, 'script', 'dataLayer', 'GTM-TM6G5JD');
+// (function (w, d, s, l, i) {
+//     w[l] = w[l] || []; 
+//     w[l].push({
+//         'gtm.start': new Date().getTime(), 
+//         event: 'gtm.js'
+//     }); 
+//     var f = d.getElementsByTagName(s)[0],
+//         j = d.createElement(s), 
+//         dl = l != 'dataLayer' ? '&l=' + l : ''; 
+//     j.async = true; 
+//     j.src = '//www.googletagmanager.com/gtm.js?id=' + i + dl; 
+//     f.parentNode.insertBefore(j, f);
+// })(window, document, 'script', 'dataLayer', 'GTM-TM6G5JD');
 
 // Lazy Load Videos
 document.addEventListener("DOMContentLoaded", function () {
